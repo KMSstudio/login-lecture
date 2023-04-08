faddbeec95d3f2ac817dcf0eee847d4ff34bd0a7
mskang <tomskang@naver.com>
2023-04-08 10:14:46 +0900

    login fir

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,40 @@
 "use strict";
 
-const home = (req, res) => {
-    res.render("home/index");
-}
+const output = {
+    home: (req, res) => {
+        res.render("home/index");
+    },
+    login: (req, res) => {
+        res.render("home/login");
+    },
+};
 
-const login = (req, res) => {
-    res.render("home/login");
-}
+const users = {
+    id: ["kms", "ttm", "serius"],
+    psword: ["1234", "1234", "123456"],
+};
+
+const process = {
+    login: (req, res) => {
+        const id = req.body.id, psword = req.body.psword;
+        console.log(id, psword);
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword){
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+        });
+    },
+};
 
 module.exports = {
-    home, //hello: hello
-    login  //login:login
+    output,
+    process,
 };
